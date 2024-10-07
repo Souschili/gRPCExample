c30bb8bc0c7ad6898d9f024a577b6edbab270c91
Orkhan <aliyevorhan82@gmail.com>
2024-10-07 05:19:42 +0400

    add console app

---
+++ UserGrpcClientApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserGrpcClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
