55850d47fcfa6fdcb34b428bbcf5fde32d93a6d4
Orkhan <aliyevorhan82@gmail.com>
2024-10-10 03:10:56 +0400

    тупое решение...

---
+++ UserGrpcService/UserExtention.cs
@@ -0,0 +1,14 @@
+﻿using SharedModels;
+using UserGrpcService.Protos;
+
+namespace UserGrpcService
+{
+    public static class UserExtention
+    {
+        public static User MapToUser(this UserResponse response) =>
+            new User { Id = response.Id, Name = response.Name, Email = response.Email };
+        public static UserResponse MapToUserResponse(this User user) =>
+             new UserResponse { Id = user.Id, Name = user.Name, Email = user.Email };
+    }
+}
+
