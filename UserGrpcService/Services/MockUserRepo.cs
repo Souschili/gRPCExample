ea3a69a1676f8dceaa36096b63acd205b7db5e24
Orkhan <aliyevorhan82@gmail.com>
2024-10-10 01:29:34 +0400

    change repo

--- UserGrpcService/Services/MockUserRepo.cs
+++ UserGrpcService/Services/MockUserRepo.cs
@@ -1,44 +1,45 @@
 ﻿using Grpc.Core;
+using SharedModels;
 using UserGrpcService.Protos;
 
 namespace UserGrpcService.Services
 {
     public class MockUserRepo
     {
-        private static List<UserResponse> _userRepo = [
-            new UserResponse{Id=1,Name="Sarhan"},
-            new UserResponse{Id=2,Name="Orhan"},
-            new UserResponse{Id=3,Name="Radj"},
-            new UserResponse{Id=4,Name="Renat"},
+        private static List<User> _userRepo = [
+            new User{Id=1,Name="Sarhan",Email="test1@demo.az"},
+            new User{Id=2,Name="Orhan",Email="test2@demo.az"},
+            new User{Id=3,Name="Radj",Email="test3@demo.az"},
+            new User{Id=4,Name="Renat",Email="test4@demo.az"},
             ];
 
-        public IEnumerable<UserResponse> All()
-        {
-            return _userRepo;
-        }
+        //public IEnumerable<UserResponse> All()
+        //{
+        //    return _userRepo;
+        //}
 
-        public void RemoveUser(IEnumerable<int> listId)
-        {
-            // если список будет содержать больше 100 элементов 
-            // то стоит рассмотреть вараинт с использованием HashSet<T> 
-            // где время поиска равно O(1) а в списке оно O(n)
-            //HashSet<int> hashSetIds = new HashSet<int>(listId);
-            //_userRepo.RemoveAll(x => hashSetIds.Contains(x.Id));
+        //public void RemoveUser(IEnumerable<int> listId)
+        //{
+        //    // если список будет содержать больше 100 элементов 
+        //    // то стоит рассмотреть вараинт с использованием HashSet<T> 
+        //    // где время поиска равно O(1) а в списке оно O(n)
+        //    //HashSet<int> hashSetIds = new HashSet<int>(listId);
+        //    //_userRepo.RemoveAll(x => hashSetIds.Contains(x.Id));
 
-            var ids = _userRepo.Select(x => x.Id);
-            _userRepo.RemoveAll(x=> ids.Contains(x.Id));
-        }
+        //    var ids = _userRepo.Select(x => x.Id);
+        //    _userRepo.RemoveAll(x=> ids.Contains(x.Id));
+        //}
 
-        public UserResponse? GetUserById(int id) =>
-            _userRepo.FirstOrDefault(x => x.Id == id);
+        //public UserResponse? GetUserById(int id) =>
+        //    _userRepo.FirstOrDefault(x => x.Id == id);
 
-        public void UpdateUser(UserUpdateRequest request)
-        {
-            var user=_userRepo.FirstOrDefault(x=> x.Id == request.Id);
-            if (user is null)
-                throw new RpcException(new Status(StatusCode.NotFound, "User not found"));
-            user.Name = request.Name;
-        }
+        //public void UpdateUser(UserUpdateRequest request)
+        //{
+        //    var user=_userRepo.FirstOrDefault(x=> x.Id == request.Id);
+        //    if (user is null)
+        //        throw new RpcException(new Status(StatusCode.NotFound, "User not found"));
+        //    user.Name = request.Name;
+        //}
 
        
     }
--- UserGrpcService/Services/UserGrpcService.cs
+++ UserGrpcService/Services/UserGrpcService.cs
@@ -13,55 +13,55 @@ namespace UserGrpcService.Services
             _userRepo = userRepo;
         }
 
-        public override async Task<Empty> DeleteUser(IAsyncStreamReader<UserIdRequest> requestStream, ServerCallContext context)
-        {
-            List<int> userIds = new List<int>();
-            await foreach (var request in requestStream.ReadAllAsync())
-            {
-                userIds.Add(request.Id);
-            }
+        //public override async Task<Empty> DeleteUser(IAsyncStreamReader<UserIdRequest> requestStream, ServerCallContext context)
+        //{
+        //    List<int> userIds = new List<int>();
+        //    await foreach (var request in requestStream.ReadAllAsync())
+        //    {
+        //        userIds.Add(request.Id);
+        //    }
 
-            if (userIds.Count == 0)
-            {
-                return new Empty();
-            }
+        //    if (userIds.Count == 0)
+        //    {
+        //        return new Empty();
+        //    }
 
-            _userRepo.RemoveUser(userIds);
-            return new Empty();
-        }
+        //    _userRepo.RemoveUser(userIds);
+        //    return new Empty();
+        //}
 
-        public override async Task GetAll(Empty request, IServerStreamWriter<UserResponse> responseStream, ServerCallContext context)
-        {
-            var users = _userRepo.All();
-            foreach (var user in users)
-            {
-                await responseStream.WriteAsync(user);
-            }
-        }
+        //public override async Task GetAll(Empty request, IServerStreamWriter<UserResponse> responseStream, ServerCallContext context)
+        //{
+        //    var users = _userRepo.All();
+        //    foreach (var user in users)
+        //    {
+        //        await responseStream.WriteAsync(user);
+        //    }
+        //}
 
-        public override Task<UserResponse> GetUserById(UserIdRequest request, ServerCallContext context)
-        {
-            var user = _userRepo.GetUserById(request.Id);
-            if (user != null)
-                return Task.FromResult(user); //так как результат уже есть то async\await ненужен 
-            // генерируем RcpException ошибку для клиента , ну и для сервера(если логировать будем)
-            throw new RpcException(new Status(StatusCode.NotFound, $"User with {request.Id} not found"));
-        }
+        //public override Task<UserResponse> GetUserById(UserIdRequest request, ServerCallContext context)
+        //{
+        //    var user = _userRepo.GetUserById(request.Id);
+        //    if (user != null)
+        //        return Task.FromResult(user); //так как результат уже есть то async\await ненужен 
+        //    // генерируем RcpException ошибку для клиента , ну и для сервера(если логировать будем)
+        //    throw new RpcException(new Status(StatusCode.NotFound, $"User with {request.Id} not found"));
+        //}
 
-        public override Task<Empty> UpdateUser(UserUpdateRequest request, ServerCallContext context)
-        {
-                _userRepo.UpdateUser(request);
-                return Task.FromResult(new Empty()); 
-        }
+        //public override Task<Empty> UpdateUser(UserUpdateRequest request, ServerCallContext context)
+        //{
+        //        _userRepo.UpdateUser(request);
+        //        return Task.FromResult(new Empty()); 
+        //}
 
-        public override async Task UserCreate(IAsyncStreamReader<UserCreateRequest> requestStream, IServerStreamWriter<UserResponse> responseStream, ServerCallContext context)
-        {
-            List<UserCreateRequest> createList=new List<UserCreateRequest>();
-            await foreach(var request in requestStream.ReadAllAsync())
-            {
-                createList.Add(request);
-            }
-            throw new RpcException(new Status(StatusCode.Unimplemented,"Not implemented"));
-        }
+        //public override async Task UserCreate(IAsyncStreamReader<UserCreateRequest> requestStream, IServerStreamWriter<UserResponse> responseStream, ServerCallContext context)
+        //{
+        //    List<UserCreateRequest> createList=new List<UserCreateRequest>();
+        //    await foreach(var request in requestStream.ReadAllAsync())
+        //    {
+        //        createList.Add(request);
+        //    }
+        //    throw new RpcException(new Status(StatusCode.Unimplemented,"Not implemented"));
+        //}
     }
 }
